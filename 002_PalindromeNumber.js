0544cf10887c8c877269d2687eda99ef4349a00a
otatakahiro <ota7281@gmail.com>
2022-11-22 18:49:32 -0800

    002_PalindromeNumber.js

---
+++ 002_PalindromeNumber.js
@@ -0,0 +1,20 @@
+let x = "kdkkjdkffkdjkkdk";
+
+var isPalindrome = function(x) {
+  x = String(x);
+  if(x < 0){
+    return false;
+  }else{
+    for(let i = 0; i < x.length; i++){
+      if(x[i] != x[x.length - i - 1]){
+        return false;
+      }
+      if(i == Math.floor(x.length / 2)){
+        return true;
+      }
+    }
+  }
+};
+console.log(isPalindrome(x));
+
+
