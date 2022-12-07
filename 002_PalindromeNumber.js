eb50a3a02166e303f171d6d243a064eb5138aad6
otatakahiro <ota7281@gmail.com>
2022-12-07 15:54:43 -0800

    12_07 review second quiz

--- 002_PalindromeNumber.js
+++ 002_PalindromeNumber.js
@@ -1,20 +1,15 @@
-let x = "kdkkjdkffkdjkkdk";
-//
-var isPalindrome = function(x) {
+//Given an integer x, return true if x is a palindrome, and false otherwise.
+
+const isPalindrome = function(x) {
   x = String(x);
-  if(x < 0){
-    return false;
-  }else{
+  if(x < 0) return false;
+  else{
     for(let i = 0; i < x.length; i++){
-      if(x[i] != x[x.length - i - 1]){
-        return false;
-      }
-      if(i == Math.floor(x.length / 2)){
-        return true;
-      }
+      if(x[i] != x[x.length - i - 1]) return false;
+      if(i == Math.floor(x.length / 2)) return true;
     }
   }
 };
-console.log(isPalindrome(x));
+console.log(isPalindrome(121));
 
 
