92fbef14eabc99ae88c77eaa9e7b93fc6619c00e
otatakahiro <ota7281@gmail.com>
2022-12-07 15:14:38 -0800

    12/07 review first quiz

--- 001_TwoSum.js
+++ 001_TwoSum.js
@@ -1,12 +1,12 @@
-let nums = [5,8,7,2];
-let target = 9;
-var twoSum = function(nums, target) {
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order
+
+const twoSum = function(nums, target){
   for(let i = 0; i < nums.length; i++){
-    for(let j = i+1; j < nums.length; j++){
-      if(target == nums[i] + nums[j]){
-        return [i, j];
-      }
+    for(let j = i + 1; j < nums.length; j++){
+      if(target == nums[i] + nums[j]) return [i, j];
     }
   }
-};
-console.log(twoSum(nums,target))
+}
+console.log(twoSum([2,7,11,15],9));
